4576a10bff3bf3d327b147acb63ce70f3d9e5fc5
Cazares7 <43894633+Cazares7@users.noreply.github.com>
2018-10-07 20:52:40 -0700

    Add files via upload
    Here are some scripts for the game, you can put them in your scripts file in unity

---
+++ Scripts/PlayerHealthManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour {
+
+    public int player_max_health;
+
+    public int player_current_health;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        player_current_health = player_max_health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player_current_health < 0)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+
+    public void HurtPlayer (int damage_to_give)
+    {
+        player_current_health -= damage_to_give;
+    }
+
+    /*public void SetMaxHealth ()
+    {
+        player_current_health = player_max_health;
+    }*/
+}
