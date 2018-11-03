e7ae741b7a381dc1ea70c21f8c8e92f072509822
Cazares7 <CazaresJMH50@gmail.com>
2018-11-03 12:53:10 -0700

    It worked

--- Assets/Scripts/UI Scripts/UIManager.cs
+++ Assets/Scripts/UI Scripts/UIManager.cs
@@ -9,6 +9,10 @@ public class UIManager : MonoBehaviour {
     public Slider healthBar;
     public Text HPText;
     public PlayerHealthManager playerHealth;
+
+    public Slider magic_bar;
+    public Text magic_bar_text;
+    public PlayerMagicManager player_magic;
     private static bool UIExists;
 
 	// Use this for initialization
@@ -33,5 +37,9 @@ public class UIManager : MonoBehaviour {
         healthBar.maxValue = playerHealth.player_max_health;
         healthBar.value = playerHealth.player_current_health;
         HPText.text = "HP: " + playerHealth.player_current_health + " / " + playerHealth.player_max_health;
+
+        magic_bar.maxValue = player_magic.player_max_magic;
+        magic_bar.value = player_magic.player_current_magic;
+        magic_bar_text.text = "MP: " + player_magic.player_current_magic + " / " + player_magic.player_max_magic;
 	}
 }
