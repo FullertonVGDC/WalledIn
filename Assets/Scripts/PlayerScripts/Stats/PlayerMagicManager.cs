cf5588d18662bcb01bae4e2b2424f7a9f9d31cd9
Cazares7 <43894633+Cazares7@users.noreply.github.com>
2018-11-03 13:01:51 -0700

    Add files via upload

---
+++ Assets/Scripts/PlayerScripts/Stats/PlayerMagicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMagicManager : MonoBehaviour {
+
+    public int player_max_magic;
+    public int player_current_magic;
+
+    // Use this for initialization
+    void Start () {
+        player_current_magic = player_max_magic;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UseMagic (int magic_to_use)
+    {
+        player_current_magic -= magic_to_use;
+    }
+}
